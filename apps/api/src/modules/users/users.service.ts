5ee688be5637fedbab7bc12caaee2f7fdbc99e7d
dev-alfaiz <captainalfaiz@gmail.com>
2026-08-04 07:41:55 +0530

    BACKEND ADD: Implement repository layer

--- apps/api/src/modules/users/users.service.ts
+++ apps/api/src/modules/users/users.service.ts
@@ -1,8 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
     private readonly logger = new Logger(UsersService.name);
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) { }
     healthCheck() {
         this.logger.debug('Health check called');
         return {
@@ -10,4 +18,17 @@ export class UsersService {
             status: "healthy",
         };
     };
+
+    createUser(dto: any) {
+        const user = this.userRepository.create(dto);
+        return this.userRepository.save(user);
+    }
+
+    findAllUsers() {
+        return this.userRepository.find();
+    }
+
+    findUserByEmail(email: string) {
+        return this.userRepository.findOne({ where: { email } });
+    }
 }
